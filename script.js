a78164b2e98dd01d1dbbb162869d751f4ef975f0
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-06-19 18:44:01 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    document.getElementById('navbarToggler').addEventListener('click', function() {
+    this.classList.toggle('collapsed');
+    });
+    
+})
